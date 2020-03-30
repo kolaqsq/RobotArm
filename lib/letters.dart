7d4dfad9e63caf1dd3a28d9031a32d32d8e02513
kolaqsq <kolaqsq1707@gmail.com>
2020-03-30 21:30:07 +0300

    Main code separated to some files

---
+++ lib/letters.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'main.dart';
+
+class LettersMain extends StatelessWidget {
+  LettersMain();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Буквы'),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,102 +1,24 @@
 import 'package:flutter/material.dart';
-import 'gestures.dart';
 
-class BottomTabBar extends StatefulWidget {
-  const BottomTabBar({Key key}) : super(key: key);
+import 'navigation.dart';
+import 'themes.dart';
 
-  @override
-  State<StatefulWidget> createState() => _BottomTabBarState();
-}
-
-class _BottomTabBarState extends State<BottomTabBar>
-    with SingleTickerProviderStateMixin {
-  TabController _tabController;
-
-  static final _kTabPages = <Widget>[
-    new GesturesMain(),
-    new LettersMain(),
-  ];
-  static const _kTabs = <Tab>[
-    Tab(
-      icon: Icon(
-        Icons.pan_tool,
-      ),
-    ),
-    Tab(
-      icon: Icon(
-        Icons.text_fields,
-      ),
-    ),
-  ];
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(
-      length: _kTabPages.length,
-      vsync: this,
-    );
-  }
-
-  @override
-  void dispose() {
-    _tabController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: null,
-      body: TabBarView(
-        children: _kTabPages,
-        controller: _tabController,
-      ),
-      bottomNavigationBar: Material(
-        color: PrimaryColor,
-        child: TabBar(
-          tabs: _kTabs,
-          controller: _tabController,
-        ),
-      ),
-    );
-  }
-}
-
-class LettersMain extends StatelessWidget {
-  LettersMain();
-
+class RoAr extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Буквы'),
-        backgroundColor: PrimaryColor,
-      ),
-      backgroundColor: PrimaryColor,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: new NavigationScreen(),
+      theme: ThemeData.light(),
+      darkTheme: themeDarkMain,
     );
   }
 }
 
 const PrimaryColor = Color(0xFF2F303B);
-const SecondaryColor = Color(0xFFC4C4C4);
+const AccentColor = Color(0xFFC4C4C4);
 const TextColor = Color(0xFFFFFFFF);
-const AccentColor = Color(0xFF7E7E7E);
+const SecondaryColor = Color(0xFF7E7E7E);
 
-void main() => runApp(new MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: new BottomTabBar(),
-      theme: new ThemeData(
-        accentColor: TextColor,
-        backgroundColor: PrimaryColor,
-        appBarTheme: new AppBarTheme(color: PrimaryColor),
-        scaffoldBackgroundColor: PrimaryColor,
-        applyElevationOverlayColor: false,
-        tabBarTheme: new TabBarTheme(
-          labelColor: TextColor,
-          unselectedLabelColor: AccentColor,
-          indicatorSize: TabBarIndicatorSize.label,
-        ),
-      ),
-    ));
+void main() => runApp(RoAr());
