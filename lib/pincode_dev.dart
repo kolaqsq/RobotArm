c0760322382842b5f34a54351e438d0b6fd9105d
kolaqsq <kolaqsq1707@gmail.com>
2020-07-02 18:21:04 +0300

    Finished auth screen design

--- lib/pincode_dev.dart
+++ lib/pincode_dev.dart
@@ -100,10 +100,278 @@ class AuthScreenState extends State<AuthScreen> {
                 ],
               ),
             ),
-            new GridView.count(
-              crossAxisCount: 3,
-              children: <Widget>[],
-            )
+            Padding(
+              padding: const EdgeInsets.only(bottom: 30.0),
+              child: new Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15.0),
+                    child: new ButtonTheme(
+                      minWidth: 75.0,
+                      height: 75.0,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(37.5),
+                          side: BorderSide(color: TextColor, width: 2)),
+                      child: new FlatButton(
+                        splashColor: AccentColor,
+                        onPressed: () {
+                          print('gunga');
+                        },
+                        child: new Text(
+                          '1',
+                          style:
+                              new TextStyle(color: TextColor, fontSize: 37.5),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15.0),
+                    child: new ButtonTheme(
+                      minWidth: 75.0,
+                      height: 75.0,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(37.5),
+                          side: BorderSide(color: TextColor, width: 2)),
+                      child: new FlatButton(
+                        splashColor: AccentColor,
+                        onPressed: () {
+                          print('gunga');
+                        },
+                        child: new Text(
+                          '2',
+                          style:
+                              new TextStyle(color: TextColor, fontSize: 37.5),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15.0),
+                    child: new ButtonTheme(
+                      minWidth: 75.0,
+                      height: 75.0,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(37.5),
+                          side: BorderSide(color: TextColor, width: 2)),
+                      child: new FlatButton(
+                        splashColor: AccentColor,
+                        onPressed: () {
+                          print('gunga');
+                        },
+                        child: new Text(
+                          '3',
+                          style:
+                              new TextStyle(color: TextColor, fontSize: 37.5),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 30.0),
+              child: new Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15.0),
+                    child: new ButtonTheme(
+                      minWidth: 75.0,
+                      height: 75.0,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(37.5),
+                          side: BorderSide(color: TextColor, width: 2)),
+                      child: new FlatButton(
+                        splashColor: AccentColor,
+                        onPressed: () {
+                          print('gunga');
+                        },
+                        child: new Text(
+                          '4',
+                          style:
+                              new TextStyle(color: TextColor, fontSize: 37.5),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15.0),
+                    child: new ButtonTheme(
+                      minWidth: 75.0,
+                      height: 75.0,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(37.5),
+                          side: BorderSide(color: TextColor, width: 2)),
+                      child: new FlatButton(
+                        splashColor: AccentColor,
+                        onPressed: () {
+                          print('gunga');
+                        },
+                        child: new Text(
+                          '5',
+                          style:
+                              new TextStyle(color: TextColor, fontSize: 37.5),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15.0),
+                    child: new ButtonTheme(
+                      minWidth: 75.0,
+                      height: 75.0,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(37.5),
+                          side: BorderSide(color: TextColor, width: 2)),
+                      child: new FlatButton(
+                        splashColor: AccentColor,
+                        onPressed: () {
+                          print('gunga');
+                        },
+                        child: new Text(
+                          '6',
+                          style:
+                              new TextStyle(color: TextColor, fontSize: 37.5),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 30.0),
+              child: new Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15.0),
+                    child: new ButtonTheme(
+                      minWidth: 75.0,
+                      height: 75.0,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(37.5),
+                          side: BorderSide(color: TextColor, width: 2)),
+                      child: new FlatButton(
+                        splashColor: AccentColor,
+                        onPressed: () {
+                          print('gunga');
+                        },
+                        child: new Text(
+                          '7',
+                          style:
+                              new TextStyle(color: TextColor, fontSize: 37.5),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15.0),
+                    child: new ButtonTheme(
+                      minWidth: 75.0,
+                      height: 75.0,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(37.5),
+                          side: BorderSide(color: TextColor, width: 2)),
+                      child: new FlatButton(
+                        splashColor: AccentColor,
+                        onPressed: () {
+                          print('gunga');
+                        },
+                        child: new Text(
+                          '8',
+                          style:
+                              new TextStyle(color: TextColor, fontSize: 37.5),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15.0),
+                    child: new ButtonTheme(
+                      minWidth: 75.0,
+                      height: 75.0,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(37.5),
+                          side: BorderSide(color: TextColor, width: 2)),
+                      child: new FlatButton(
+                        splashColor: AccentColor,
+                        onPressed: () {
+                          print('gunga');
+                        },
+                        child: new Text(
+                          '9',
+                          style:
+                              new TextStyle(color: TextColor, fontSize: 37.5),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 30.0),
+              child: new Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15.0),
+                    child: new Container(
+                      width: 75.0,
+                      height: 75.0,
+                      color: PrimaryColor,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15.0),
+                    child: new ButtonTheme(
+                      minWidth: 75.0,
+                      height: 75.0,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(37.5),
+                          side: BorderSide(color: TextColor, width: 2)),
+                      child: new FlatButton(
+                        splashColor: AccentColor,
+                        onPressed: () {
+                          print('gunga');
+                        },
+                        child: new Text(
+                          '0',
+                          style:
+                              new TextStyle(color: TextColor, fontSize: 37.5),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15.0),
+                    child: new Ink(
+                      width: 75.0,
+                      height: 75.0,
+                      decoration: new BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: PrimaryColor,
+                          border: Border.all(width: 2.0, color: TextColor)),
+                      child: new IconButton(
+                        splashColor: AccentColor,
+                        onPressed: () {
+                          print('gunga');
+                        },
+                        icon: Icon(
+                          Icons.chevron_left,
+                          size: 37.5,
+                          color: TextColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
